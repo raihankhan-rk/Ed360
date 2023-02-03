7b7a368c37a1c05b0444fd40ade7e263f22e2230
SayakSarkar02 <sayaksarkar73@gmail.com>
2023-02-04 02:03:10 +0530

    fixes morree

---
+++ teacherfrontend/components/utils/notice.jsx
@@ -0,0 +1,30 @@
+import Nodelete from "../textareanodelete";
+import Textarea from "./textarea";
+import React from "react";
+
+
+
+function Notice(props){
+    const resp = props.notice;
+    console.log("noticesssss: ", resp.notices);
+    // complete array of notices is passed as props
+    const noticeArray = resp.notices || [];
+    console.log("noticeArray: ", noticeArray);
+    
+    return <div className="w-11/12 mx-auto my-[2rem]">
+
+        {/* mapping is done here */}
+        {noticeArray.slice(0).reverse().map((notice) => (
+
+            // each card
+            <div className="bg-pale text-lg rounded-2xl my-4 p-8">
+                <p className="whitespace-pre-wrap my-2 font-[Montserrat] text-accent">{notice.data}</p>
+                {notice.fileURL===null?null:<img className="w-4/12 my-2" src={notice.fileURL} alt="file" />}
+                <h6 className="font-[Montserrat] text-primary">{notice.time} {notice.date}</h6>
+            </div>
+            ))}
+        
+    </div>
+}
+
+export default Notice;
