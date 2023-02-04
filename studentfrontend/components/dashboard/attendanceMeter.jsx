3fcfcb9e849007e229054725fd4c5ed344c1d94a
Risav03 <needleweed03@gmail.com>
2023-02-04 17:55:20 +0530

    new

--- studentfrontend/components/dashboard/attendanceMeter.jsx
+++ studentfrontend/components/dashboard/attendanceMeter.jsx
@@ -1,25 +1,29 @@
 import React from 'react'
 import axios from 'axios'
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import Image from 'next/image'
 
 const baseURL = "https://ed360-student-backend.vercel.app/api/student/apyhub/"
 
 export default function AttendanceMeter(props) {
+  const [link,setLink] = useState("");
   
   // const [post, setPost] = useState([]);
-
-  axios.post(baseURL, {
-    present: '10',
-    absent: '90'
-  }).then((response) => {
-    console.log(response.data);
-  }, (error) => {
-    console.log(error);
-  });
+  useEffect(() => {
+    axios.post(baseURL, {
+      present: '10',
+      absent: '90'
+    }).then((response) => {
+      console.log(response.data);
+      setLink(response.data)
+      response
+    }, (error) => {
+      console.log(error);
+    });
+  }, []);
 
   return (<div className='pb-20 w-10/12 mx-auto text-center'>
-    {/* <Image src={res} className="w-4/12"></Image> */}
+    <Image src={link} className="w-4/12"></Image>
     </div>
   )
 }
